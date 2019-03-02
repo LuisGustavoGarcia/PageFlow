6589377087806fa131ec0e1225820964d9db5660
Luis Garcia <garcialuis1998@yahoo.es>
2019-03-02 12:34:06 -0500

    Added some back-end code for js file interaction.

--- background.js
+++ background.js
@@ -1,32 +1,30 @@
 // This file will handle speech and other chrome add-on functionality.
 
+// Read the response from the user, and continue.
+function readMsg(msg){
+    if(msg.tts != "" && msg.tts != null){
+        readString(msg.tts, msg.callback);
+    }
+}
 
-// How to send messages back and forth between background.js and page-parser.js
+/* Uses the text-to-speech engine, then calls a chosen callback when string has been read */
+function readString(dataStr, readStringCallback){
+    chrome.tts.speak(dataStr, {
+        requiredEventTypes: ['end'],
+        onEvent: function(event) {
+            if(event.type === 'end') {
+                readStringCallback();
+            }
+        }
+    });
+}
 
-// chrome.runtime.onConnect.addListener(function(port) {
-//     console.assert(port.name == "joke");
-//     port.onMessage.addListener(function(msg) {
-//       if (msg.joke == "Why are Canadians so good at sports?")
-//       {
-//         chrome.tts.speak(msg.joke, {
-//             requiredEventTypes: ['end'],
-//             onEvent: function(event) {
-//                 if(event.type === 'end') {
-//                     port.postMessage({question: "Why?"});
-//                 }
-//             }
-//         });
-//       }
-//       else if (msg.answer == "They always bring their eh game")
-//       {
-//         chrome.tts.speak(msg.answer, {
-//             requiredEventTypes: ['end'],
-//             onEvent: function(event) {
-//                 if(event.type === 'end') {
-//                     port.postMessage({response: "That sucked."});
-//                 }
-//             }
-//         });
-//       }
-//     });
-// });
+/* ================ Add Functions Above This ================ */
+
+/* This communicates with the page-parser.js file */
+chrome.runtime.onConnect.addListener(function(port) {
+    console.assert(port.name == "userInteraction");
+    port.onMessage.addListener(function(msg){
+        readMsg(msg);
+    });
+});
--- page-parser.js
+++ page-parser.js
@@ -1,16 +1,14 @@
 // This file will handle DOM parsing
 
-// Use Jquery to write to the end of the body tag.
-//$("body").append("TEST");
+/* Create a port to send messages back and forth between background.js and page-parser.js */
+var port = chrome.runtime.connect({name: "userInteraction"});
+var nextToRead;
 
+/* Respond to messages from background.js here */
+port.onMessage.addListener(function(msg){});
 
-// How to send messages back and forth between background.js and page-parser.js
-// var port = chrome.runtime.connect({name: "joke"});
-// port.postMessage({joke: "Why are Canadians so good at sports?"});
-// port.onMessage.addListener(function(msg) {
-//   if (msg.question == "Why?")
-//   {
-//     console.log("This ran1");
-//     port.postMessage({answer: "They always bring their eh game"});
-//   }
-// });
+/* Send data to be read by the text-to-speech engine, and a callback function to execute afterwards */
+/* Note: the callback function must be located within background.js, NOT page-parser.js */
+function sendReadData(dataStr, readCallback){
+    port.postMessage({tts: dataStr, callback: readCallback});
+}
