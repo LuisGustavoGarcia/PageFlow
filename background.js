0b5ca1e2c30dfa8532f0f9fd6d3d6c00dfd94eba
Luis Garcia <garcialuis1998@yahoo.es>
2019-03-02 06:38:08 -0500

    Added voice functionality.

--- background.js
+++ background.js
@@ -1,2 +1,10 @@
-// Use Jquery to write to the end of the body tag.
-// $("body").append("TEST");
+// This file will handle speech and other chrome add-on functionality.
+
+// var i = 0
+// while(i < 2){
+//     chrome.tts.speak("Hello, Debrian.");
+//     chrome.tts.isSpeaking(function(){
+//         Console.log("Speaking...");
+//     })
+//     i+=1;
+// }
---
+++ page-parser.js
@@ -0,0 +1,4 @@
+// This file will handle DOM parsing
+
+// Use Jquery to write to the end of the body tag.
+//$("body").append("TEST");
