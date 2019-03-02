1f24f7a9b0d9a8e00cc5e070c345077de1761a0d
Luis Garcia <garcialuis1998@yahoo.es>
2019-03-02 08:37:23 -0500

    Found how to communicate between background and content scripts.

--- background.js
+++ background.js
@@ -1,10 +1,32 @@
 // This file will handle speech and other chrome add-on functionality.
 
-// var i = 0
-// while(i < 2){
-//     chrome.tts.speak("Hello, Debrian.");
-//     chrome.tts.isSpeaking(function(){
-//         Console.log("Speaking...");
-//     })
-//     i+=1;
-// }
+
+// How to send messages back and forth between background.js and page-parser.js
+
+// chrome.runtime.onConnect.addListener(function(port) {
+//     console.assert(port.name == "joke");
+//     port.onMessage.addListener(function(msg) {
+//       if (msg.joke == "Why are Canadians so good at sports?")
+//       {
+//         chrome.tts.speak(msg.joke, {
+//             requiredEventTypes: ['end'],
+//             onEvent: function(event) {
+//                 if(event.type === 'end') {
+//                     port.postMessage({question: "Why?"});
+//                 }
+//             }
+//         });
+//       }
+//       else if (msg.answer == "They always bring their eh game")
+//       {
+//         chrome.tts.speak(msg.answer, {
+//             requiredEventTypes: ['end'],
+//             onEvent: function(event) {
+//                 if(event.type === 'end') {
+//                     port.postMessage({response: "That sucked."});
+//                 }
+//             }
+//         });
+//       }
+//     });
+// });
--- page-parser.js
+++ page-parser.js
@@ -2,3 +2,15 @@
 
 // Use Jquery to write to the end of the body tag.
 //$("body").append("TEST");
+
+
+// How to send messages back and forth between background.js and page-parser.js
+// var port = chrome.runtime.connect({name: "joke"});
+// port.postMessage({joke: "Why are Canadians so good at sports?"});
+// port.onMessage.addListener(function(msg) {
+//   if (msg.question == "Why?")
+//   {
+//     console.log("This ran1");
+//     port.postMessage({answer: "They always bring their eh game"});
+//   }
+// });
